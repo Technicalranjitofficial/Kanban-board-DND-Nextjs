73b80b86e32258e0e22dedb8f206f9bc47f0ec9f
technicalranjitofficial <dranjitkumar16@gmail.com>
2023-10-21 11:07:42 +0530

    Kanban Board

---
+++ app/data/dummy.ts
@@ -0,0 +1,129 @@
+import { Column, Task } from "../types";
+
+//dummy column
+export const columnsDefault: Column[] = [
+    {
+      id: 1,
+      title: "Backlog",
+    },
+    {
+      id: 2,
+      title: "To Do",
+    },
+    {
+      id: 3,
+      title: "In Progress",
+    },
+    {
+      id: 4,
+      title: "Done",
+    },
+  ];
+  
+  //dummy task data
+  
+  export const tasksDefault: Task[] = [
+    {
+      id: 90,
+      title: "task1",
+      columnId: 1,
+      description: "Hello Guys",
+    },
+    {
+      id: 91,
+      title: "task2 ",
+      columnId: 2,
+      description: "Task 2",
+    },
+    {
+      id: 92,
+      title: "task3",
+      columnId: 3,
+      description: "Task 3",
+    },
+    {
+      id: 93,
+      title: "task4",
+      columnId: 4,
+      description: "Task 4",
+    },
+    {
+      id: 94,
+      title: "task5",
+      columnId: 1,
+      description: "Task 5",
+    },
+    {
+      id: 95,
+      title: "task6",
+      columnId: 2,
+      description: "Task 6",
+    },
+    {
+      id: 96,
+      title: "task7",
+      columnId: 3,
+      description: "Task 7",
+    },
+    {
+      id: 97,
+      title: "task8",
+      columnId: 4,
+      description: "Task 8",
+    },
+    {
+      id: 98,
+      title: "task9",
+      columnId: 1,
+      description: "Task 9",
+    },
+    {
+      id: 99,
+      title: "task10",
+      columnId: 2,
+      description: "Task 10",
+    },
+    {
+      id: 100,
+      title: "task11",
+      columnId: 3,
+      description: "Task 11",
+    },
+    {
+      id: 101,
+      title: "task12",
+      columnId: 4,
+      description: "Task 12",
+    },
+    {
+      id: 102,
+      title: "task13",
+      columnId: 1,
+      description: "Task 13",
+    },
+    {
+      id: 103,
+      title: "task14",
+      columnId: 2,
+      description: "Task 14",
+    },
+    {
+      id: 104,
+      title: "task15",
+      columnId: 3,
+      description: "Task 15",
+    },
+    {
+      id: 105,
+      title: "task16",
+      columnId: 4,
+      description: "Task 16",
+    },
+    {
+      id: 106,
+      title: "task17",
+      columnId: 1,
+      description: "Task 17",
+    },
+  ];
+  
